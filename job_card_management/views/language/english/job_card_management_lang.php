0a14541f185a61fa66c10ffd347d0c2c6e0fa5d8
pavan more <87747114+pavangmore@users.noreply.github.com>
2025-07-04 18:09:47 +0530

    Remove old job card module copy and organize module

--- job_card_management/views/language/english/job_card_management_lang.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$lang['job_card_management'] = 'Job Card Management';
-$lang['permission_view'] = 'View Job Cards';
-$lang['permission_create'] = 'Create Job Cards';
-$lang['permission_edit'] = 'Edit Job Cards';
-$lang['permission_delete'] = 'Delete Job Cards';
